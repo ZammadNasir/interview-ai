b70bd2a2035dfbedc2f8484a4b264ea0a537e51f
Zammad Nasir <zammadnasir@gmail.com>
2026-03-04 02:52:37 +0500

    implemented user auth apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import authRouter from './routes/auth.routes.js';
 
 
 const app = express();
 
 app.use(express.json());
 
+/* using all the routes here */
+app.use('/api/auth', authRouter);
 
 export default app;
